e1e2976ca86bc1c596943209e7a11270b3c0407e
Lukasz Kallas <lukasz.kallas@gmail.com>
2022-09-22 11:33:46 +0200

    Add button

--- src/js/components/Game.js
+++ src/js/components/Game.js
@@ -11,13 +11,32 @@ class Game {
 		const thisGame = this;
 		thisGame.dom = {};
 		thisGame.dom.board = document.querySelector(select.gameElements.board);
+		thisGame.dom.button = document.querySelector(select.gameElements.button);
 	}
+
 	initActions() {
 		const thisGame = this;
-		thisGame.dom.board.addEventListener('click', function (event) {
-			thisGame.checkCell(event.target.getAttribute(select.gameElements.cellId));
-		});
 		thisGame.activeCells = [];
+		thisGame.dom.board.addEventListener('click', function (event) {
+			const buttonState = thisGame.dom.button.innerHTML;
+			const cellId = event.target.getAttribute(select.gameElements.cellId);
+			thisGame.dom.checkedCell = thisGame.dom.board.querySelector(`[data-id="${cellId}"]`);
+			if (buttonState == settings.button.start) {
+				thisGame.addCell(cellId);
+			} else if (buttonState == settings.button.end) {
+				thisGame.selectPoints(cellId);
+			}
+		});
+		thisGame.dom.button.addEventListener('click', function () {
+			const buttonState = thisGame.dom.button.innerHTML;
+			if (buttonState == settings.button.start) {
+				thisGame.dom.button.innerHTML = settings.button.end;
+			}
+			if (buttonState == settings.button.end && thisGame.startPoint >= 0 && thisGame.endPoint >= 0) {
+				thisGame.dom.button.innerHTML = settings.button.reset;
+				thisGame.calcRoutes();
+			}
+		});
 	}
 	initBorders() {
 		const thisGame = this;
@@ -33,9 +52,9 @@ class Game {
 			thisGame.borderLeft.push(i);
 		}
 	}
-	checkCell(cellId) {
+
+	addCell(cellId) {
 		const thisGame = this;
-		thisGame.dom.checkedCell = thisGame.dom.board.querySelector(`[data-id="${cellId}"]`);
 		const checkedCell = thisGame.dom.checkedCell;
 		if (thisGame.activeCells.length === 0) {
 			checkedCell.classList.add(select.styles.active);
@@ -68,7 +87,12 @@ class Game {
 		options.forEach(function (element) {
 			const cell = parseInt(cellId) + element;
 			const optionCell = thisGame.dom.board.querySelector(`[data-id="${cell}"]`);
-			if (cell >= 0 && cell < 100 && !optionCell.classList.contains(select.styles.active) && !optionCell.classList.contains(select.styles.option)) {
+			if (
+				cell >= 0 &&
+				cell < settings.game.rows * settings.game.cols &&
+				!optionCell.classList.contains(select.styles.active) &&
+				!optionCell.classList.contains(select.styles.option)
+			) {
 				optionCell.classList.add(select.styles.option);
 				thisGame.lastOptions.push(cell);
 			}
@@ -89,6 +113,54 @@ class Game {
 			const lastCell = thisGame.dom.board.querySelector(`[data-id="${thisGame.lastCell}"]`);
 			lastCell.classList.add(select.styles.option);
 		}
+	}
+	selectPoints(cellId) {
+		const thisGame = this;
+		if (thisGame.startPoint == undefined && thisGame.activeCells.includes(cellId)) {
+			thisGame.startPoint = cellId;
+			thisGame.dom.checkedCell.classList.remove(select.styles.active);
+			thisGame.dom.checkedCell.classList.add(select.styles.startPoint);
+		} else if (thisGame.startPoint >= 0 && thisGame.startPoint != cellId && thisGame.endPoint == undefined && thisGame.activeCells.includes(cellId)) {
+			thisGame.endPoint = cellId;
+			thisGame.dom.checkedCell.classList.remove(select.styles.active);
+			thisGame.dom.checkedCell.classList.add(select.styles.endPoint);
+		}
+	}
+
+	calcRoutes() {
+		const thisGame = this;
+		thisGame.activeCells.splice(thisGame.activeCells.indexOf(thisGame.startPoint), 1);
+		thisGame.activeCells.splice(thisGame.activeCells.indexOf(thisGame.endPoint), 1);
+		thisGame.cells = [];
+		thisGame.activeCells.forEach(function (element) {
+			thisGame.cells.push(parseInt(element));
+		});
+		console.log(thisGame.cells);
+		console.log(thisGame.findNext(thisGame.startPoint));
+	}
+	findNext(point) {
+		const thisGame = this;
+		point = parseInt(point);
+		let options = [-settings.game.rows, settings.game.rows];
+		let nextPoint = [];
+		console.log(thisGame.borderRight);
+		console.log(thisGame.borderLeft);
+		console.log(point);
+		if (thisGame.borderRight.includes(point)) {
+			options.push(-1);
+		} else if (thisGame.borderLeft.includes(point)) {
+			options.push(1);
+		} else {
+			options.push(-1, 1);
+		}
+		for (let option of options) {
+			const x = point + option;
+			if (x >= 0 && x < settings.game.rows * settings.game.cols && thisGame.cells.includes(x)) {
+				nextPoint.push(x);
+			}
+		}
+
+		return nextPoint;
 	}
 }
 
--- src/js/settings.js
+++ src/js/settings.js
@@ -3,15 +3,23 @@ export const select = {
 		board: '.game-board',
 		cell: '.game-cell',
 		cellId: 'data-id',
+		button: '.game-btn',
 	},
 	styles: {
 		active: 'active',
 		option: 'option',
+		startPoint: 'start',
+		endPoint: 'end',
 	},
 };
 export const settings = {
 	game: {
 		rows: 10,
 		cols: 10,
+	},
+	button: {
+		start: 'FINISH DRAWING',
+		end: 'COMPUTE',
+		reset: 'START AGAIN',
 	},
 };
